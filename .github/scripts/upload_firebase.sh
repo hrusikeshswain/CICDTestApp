5c0e08ff5fc46d85f05f971d2dd0d82a0799c9ae
Hrusikesh Swain <hemantaswn@gmail.com>
2025-09-10 12:35:48 +0530

    ipa extention update again minor

--- .github/scripts/upload_firebase.sh
+++ .github/scripts/upload_firebase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install -g firebase-tools
-firebase appdistribution:distribute CICDTestApp.app.zip \
+firebase appdistribution:distribute CICDTestApp.ipa \
   --app "$IOS_APP_ID" \
   --token "$FIREBASE_TOKEN" \
   --release-notes "Simulator build only - not installable on devices"
